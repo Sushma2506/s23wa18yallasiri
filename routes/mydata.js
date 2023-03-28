01b2da3b7613a2ca234ce62450f5511ad206260d
Sushma2506 <122947550+Sushma2506@users.noreply.github.com>
2023-03-27 19:45:34 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET my fact page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: "Sushma's fact page"});
+  res.render('mydata', { title: "Yallasiri Sai Sushma"});
 });
 
 module.exports = router;
